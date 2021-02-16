0df79c2e9168defb6e2d46d993a2d307bcfd812a
MarceloLaser <marcelo.laser@gmail.com>
2021-02-16 01:29:21 -0800

    Cleaning up Mojo a bit

--- src/main/java/mojo/BipartiteGraph.java
+++ src/main/java/mojo/BipartiteGraph.java
@@ -1,33 +1,28 @@
 package mojo;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /* Vertex in a bipartite graph */
 class Vertex {
-
     boolean matched = false;
-
     boolean isLeft = false;
-
     int outdegree = 0;
-
     int indegree = 0;
 }
 
 class BipartiteGraph {
-
     /*
      * we use a vector to represent a edge list in a directed graph for example,
      * adjacentList[1] has 2 means there is a edge from point 1 to point 2
      */
-
-    Vector<Vector<Integer>> adjacentList;
+    List<List<Integer>> adjacentList;
 
     /* vertex list */
     Vertex vertex[];
 
     /* this list is used to store the augmenting Path we got from matching */
-    Vector<Integer> augmentPath;
+    List<Integer> augmentPath;
 
     /* total number of all points,points in left side and right side */
     int points, leftpoints, rightpoints;
@@ -38,15 +33,14 @@ class BipartiteGraph {
         this.rightpoints = rightpoints;
         this.points = points;
 
-        adjacentList = new Vector<Vector<Integer>>(points);
+        adjacentList = new ArrayList<>(points);
         vertex = new Vertex[points];
-        augmentPath = new Vector<Integer>();
+        augmentPath = new ArrayList<>();
 
-        for (int i = 0; i < points; i++)
-        {
+        for (int i = 0; i < points; i++) {
             vertex[i] = new Vertex();
             if (i < leftpoints) vertex[i].isLeft = true;
-            adjacentList.add(i, new Vector<Integer>());
+            adjacentList.add(i, new ArrayList<>());
         }
 
     }
@@ -54,7 +48,7 @@ class BipartiteGraph {
     /* add edge, add an edge to the graph */
     public void addedge(int startPoint, int endPoint) {
         /* insert the edge to the adjacentList of startPoint */
-        adjacentList.elementAt(startPoint).add(new Integer(endPoint));
+        adjacentList.get(startPoint).add(endPoint);
         /* increase the outdegree of startPoint, indegree of endPoint */
         vertex[startPoint].outdegree += 1;
         vertex[endPoint].indegree += 1;
@@ -72,9 +66,9 @@ class BipartiteGraph {
 
     public void removeEdge(int startPoint, int endPoint) {
         /* find the index of edge in the adjacentList of startPoint */
-        int index = adjacentList.elementAt(startPoint).indexOf(new Integer(endPoint));
+        int index = adjacentList.get(startPoint).indexOf(endPoint);
         /* remove the edge from adjacentList of startPoint */
-        if (index > -1) adjacentList.elementAt(startPoint).removeElementAt(index);
+        if (index > -1) adjacentList.get(startPoint).remove(index);
         /* decrease the outdegree of startPoint and indegree of endPoint */
         vertex[startPoint].outdegree -= 1;
         vertex[endPoint].indegree -= 1;
@@ -104,11 +98,11 @@ class BipartiteGraph {
         int start, end;
         String str = "";
         /* the first point of augmenting path */
-        start = augmentPath.elementAt(0).intValue();
+        start = augmentPath.get(0).intValue();
 
         for (int i = 1; i < augmentPath.size(); i++)
         {
-            end = augmentPath.elementAt(i).intValue();
+            end = augmentPath.get(i).intValue();
             reverseEdge(start, end);
             start = end;
         }
@@ -119,25 +113,22 @@ class BipartiteGraph {
     public String matching() {
 
         String str = "";
-        while (findAugmentPath())
-        {
+        while (findAugmentPath()) {
             str += XOR();
         }
         return str;
     }
 
     public boolean findAugmentPath() {
-        augmentPath.removeAllElements(); /* init the path */
+        augmentPath.clear(); /* init the path */
         /*
          * use all the unmatched left points as start, see if we can find a
          * augmenting path
          */
-        for (int i = 0; i < leftpoints; i++)
-        {
-            if (vertex[i].matched == false)
-            {
+        for (int i = 0; i < leftpoints; i++) {
+            if (!vertex[i].matched) {
                 if (findPath(i)) return true;
-                else augmentPath.removeAllElements(); /* re init the path */
+                else augmentPath.clear(); /* re init the path */
             }
         }
         return false;
@@ -149,58 +140,51 @@ class BipartiteGraph {
         /* if the current vertex has no out edge, return false */
         if (vertex[start].outdegree == 0) return false;
         /* insert the current point to the path */
-        augmentPath.addElement(new Integer(start));
+        augmentPath.add(start);
 
         /*
          * use the pts that the current point is linked to as next point,
          * recursively call findPath function
          */
-        for (int i = 0; i < adjacentList.elementAt(start).size(); i++)
-        {
-            nextPt = adjacentList.elementAt(start).elementAt(i).intValue();
+        for (int i = 0; i < adjacentList.get(start).size(); i++) {
+            nextPt = adjacentList.get(start).get(i).intValue();
             /* if the next point was already in the path, discard it */
-            if (augmentPath.indexOf(new Integer(nextPt)) > -1) continue;
+            if (augmentPath.indexOf(nextPt) > -1) continue;
             /* find a terminal, add it to the path and return true */
-            if (vertex[nextPt].matched == false)
-            {
-                augmentPath.addElement(new Integer(nextPt));
+            if (!vertex[nextPt].matched) {
+                augmentPath.add(nextPt);
                 return true;
             }
             /* otherwise recursive call using depth first search */
             else if (findPath(nextPt)) return true;
-
         }
         /* if failed, delete the current pt from path and return false */
-        index = augmentPath.indexOf(new Integer(start));
-        augmentPath.removeElementAt(index);
+        index = augmentPath.indexOf(start);
+        augmentPath.remove(index);
         return false;
 
     }
 
     /* indicate whether the current point is in right side */
     public boolean isLeft(int pt) {
-        if (pt < leftpoints) return true;
-        else return false;
+        return pt < leftpoints;
     }
 
     /* indicate whether the current point is in right side */
     public boolean isRight(int pt) {
-        if (pt > leftpoints - 1) return true;
-        else return false;
+        return pt > leftpoints - 1;
     }
 
     /* print out the current status of the graph */
     public String toString() {
         String str = "";
-        for (int i = 0; i < points; i++)
-        {
+        for (int i = 0; i < points; i++) {
             str += "Point ";
-            str += isLeft(i) == true ? "A" + (i + 1) : "G" + (i - leftpoints + 1);
+            str += isLeft(i) ? "A" + (i + 1) : "G" + (i - leftpoints + 1);
             str += " is ";
-            str += vertex[i].matched == true ? "MATCHED\n" : "UNMATCHED\n";
-            for (int j = 0; j < adjacentList.elementAt(i).size(); j++)
-            {
-                int to = adjacentList.elementAt(i).elementAt(j).intValue();
+            str += vertex[i].matched ? "MATCHED\n" : "UNMATCHED\n";
+            for (int j = 0; j < adjacentList.get(i).size(); j++) {
+                int to = adjacentList.get(i).get(j).intValue();
                 str += " and is connected to points ";
                 str += isLeft(to) ? "A" + (to + 1) : "G" + (to - leftpoints + 1);
                 str += "\n";
@@ -208,5 +192,4 @@ class BipartiteGraph {
         }
         return str;
     }
-
 }
