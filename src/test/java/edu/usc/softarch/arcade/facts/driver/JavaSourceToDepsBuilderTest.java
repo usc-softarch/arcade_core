f2dee40fc0f4592fab3a7f9ab9454e62e3c7cce4
Gina Yang <ginayang@usc.edu>
2021-02-28 00:54:14 -0800

    added struts2 fact extractor tests, oracles

--- src/test/java/edu/usc/softarch/arcade/facts/driver/JavaSourceToDepsBuilderTest.java
+++ src/test/java/edu/usc/softarch/arcade/facts/driver/JavaSourceToDepsBuilderTest.java
@@ -14,12 +14,41 @@ import edu.usc.softarch.arcade.util.RsfCompare;
 public class JavaSourceToDepsBuilderTest {
   // #region TESTS build -------------------------------------------------------
   @Test
+  public void buildTestOld() {
+    // Builds the dependencies RSF file for Josh's ARCADE version
+    char fs = File.separatorChar;
+    String classesDirPath = "." + fs + "src" + fs + "test" + fs + "resources"
+      + fs + "JavaSourceToDepsBuilderTest_resources_old"
+      + fs + "arcade_old_binaries";
+    String depsRsfFilename = "." + fs + "target" + fs + "test_results" + fs 
+      + "JavaSourceToDepsBuilderTest" + fs + "buildTestOldResult.rsf";
+
+    // Run JavaSourceToDepsBuilder.build()
+    assertDoesNotThrow(() -> (
+      new JavaSourceToDepsBuilder()).build(classesDirPath, depsRsfFilename));
+    String result = assertDoesNotThrow(() -> 
+      { return FileUtil.readFile(depsRsfFilename, StandardCharsets.UTF_8); });
+    
+    // Load oracle
+    String oraclePath = "." + fs + "src" + fs + "test" + fs + "resources"
+      + fs + "JavaSourceToDepsBuilderTest_resources_old"
+      + fs + "arcade_old_deps_oracle.rsf";
+    String oracle = assertDoesNotThrow(() ->
+      { return FileUtil.readFile(oraclePath, StandardCharsets.UTF_8); });
+    
+    // Check oracle
+    RsfCompare resultRsf = new RsfCompare(result);
+    RsfCompare oracleRsf = new RsfCompare(oracle);
+    // RsfCompare.compareTo returns 0 if files have the same contents
+    assertEquals(resultRsf.compareTo(oracleRsf), 0);
+  }
+  @Test
   public void buildTest1() {
     // Builds the dependencies RSF file for Josh's ARCADE version
     char fs = File.separatorChar;
     String classesDirPath = "." + fs + "src" + fs + "test" + fs + "resources"
       + fs + "JavaSourceToDepsBuilderTest_resources"
-      + fs + "arcade_old_binaries";
+      + fs + "binaries" + fs + "struts-2.3.30" + fs + "lib_struts";
     String depsRsfFilename = "." + fs + "target" + fs + "test_results" + fs 
       + "JavaSourceToDepsBuilderTest" + fs + "buildTest1Result.rsf";
 
@@ -32,7 +61,37 @@ public class JavaSourceToDepsBuilderTest {
     // Load oracle
     String oraclePath = "." + fs + "src" + fs + "test" + fs + "resources"
       + fs + "JavaSourceToDepsBuilderTest_resources"
-      + fs + "arcade_old_deps_oracle.rsf";
+      + fs + "struts-2.3.30_deps.rsf";
+    String oracle = assertDoesNotThrow(() ->
+      { return FileUtil.readFile(oraclePath, StandardCharsets.UTF_8); });
+    
+    // Check oracle
+    RsfCompare resultRsf = new RsfCompare(result);
+    RsfCompare oracleRsf = new RsfCompare(oracle);
+    // RsfCompare.compareTo returns 0 if files have the same contents
+    assertEquals(resultRsf.compareTo(oracleRsf), 0);
+  }
+
+  @Test
+  public void buildTest2() {
+    // Builds the dependencies RSF file for Josh's ARCADE version
+    char fs = File.separatorChar;
+    String classesDirPath = "." + fs + "src" + fs + "test" + fs + "resources"
+      + fs + "JavaSourceToDepsBuilderTest_resources"
+      + fs + "binaries" + fs + "struts-2.5.2" + fs + "lib_struts";
+    String depsRsfFilename = "." + fs + "target" + fs + "test_results" + fs 
+      + "JavaSourceToDepsBuilderTest" + fs + "buildTest2Result.rsf";
+
+    // Run JavaSourceToDepsBuilder.build()
+    assertDoesNotThrow(() -> (
+      new JavaSourceToDepsBuilder()).build(classesDirPath, depsRsfFilename));
+    String result = assertDoesNotThrow(() -> 
+      { return FileUtil.readFile(depsRsfFilename, StandardCharsets.UTF_8); });
+    
+    // Load oracle
+    String oraclePath = "." + fs + "src" + fs + "test" + fs + "resources"
+      + fs + "JavaSourceToDepsBuilderTest_resources"
+      + fs + "struts-2.5.2_deps.rsf";
     String oracle = assertDoesNotThrow(() ->
       { return FileUtil.readFile(oraclePath, StandardCharsets.UTF_8); });
     
