c1287c58e7e59a9114decfdb15ec3ac4896c1b03
Gina Yang <ginayang@usc.edu>
2021-02-27 23:04:15 -0800

    moved over and cleaned up RsfCompare

---
+++ src/main/java/edu/usc/softarch/arcade/util/RsfCompare.java
@@ -0,0 +1,26 @@
+package edu.usc.softarch.arcade.util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RsfCompare implements Comparable<RsfCompare> {
+    private Set<String> rsfSet;
+	
+    // Constructor passes in string (pass in path to FileUtil.readFile)
+	public RsfCompare(String rsfContents) {
+        this.rsfSet = new HashSet<>(Arrays.asList(rsfContents.split(System.lineSeparator())));
+	}
+	
+	@Override
+	public int compareTo(RsfCompare rsf1) {
+        // Returns 0 if contents of the 2 rsf files are the same (regardless of order)
+		if (this.rsfSet.equals(rsf1.rsfSet)) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+}
+
