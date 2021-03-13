43388f430291e2d733187c42e137e9c7d26a31d0
Gina Yang <ginayang@usc.edu>
2021-03-12 21:03:33 -0800

    wip: parameterize PipeExtractorTest (integration test)

--- src/test/java/edu/usc/softarch/arcade/util/ldasupport/PipeExtractorTest.java
+++ src/test/java/edu/usc/softarch/arcade/util/ldasupport/PipeExtractorTest.java
@@ -12,12 +12,14 @@ import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import cc.mallet.types.Instance;
 import cc.mallet.types.InstanceList;
 
 public class PipeExtractorTest {
-    // Get and save the present working directory
+    // Save the present working directory (it's at the root of the repository)
     String pwd = System.getProperty("user.dir");
 
     @BeforeEach
@@ -31,24 +33,35 @@ public class PipeExtractorTest {
         System.setProperty("user.dir", pwd + fs + "src" + fs + "main" + fs + "resources" + fs);
     }
 
-    @Test
-    public void mainTest(){
+    @ParameterizedTest
+    @CsvSource({
+        // Test parameters: [path to system version], [desired output path], [path to oracle pipe file], [system language]
+        // struts 2.3.30
+        "///src///test///resources///JavaSourceToDepsBuilderTest_resources///binaries///struts-2.3.30/lib_struts,"
+        + "///target///test_results///PipeExtractorTest///,"
+        + "///src///test///resources///PipeExtractorTest_resources///Struts2///arc///base///struts-2.3.30///output.pipe,"
+        + "java",
+        // struts 2.5.2
+        "///src///test///resources///JavaSourceToDepsBuilderTest_resources///binaries///struts-2.5.2/lib_struts,"
+        + "///target///test_results///PipeExtractorTest///,"
+        + "///src///test///resources///PipeExtractorTest_resources///Struts2///arc///base///struts-2.5.2///output.pipe,"
+        + "java",
+    })
+    public void mainTest(String versionDir, String outputDir, String oracleFile, String language){
         /** Integration test for PipeExtractor **/
-        String classesDir = pwd + "/src/test/resources/JavaSourceToDepsBuilderTest_resources/binaries/struts-2.3.30/lib_struts";
-        String resultDir = pwd + "/target/test_results/PipeExtractorTest/";
+        String classesDir = pwd + versionDir.replace("///", File.separator);
+        String resultDir = pwd + outputDir.replace("///", File.separator);
         // Path to oracle pipe file
-        String oraclePath = pwd + "/src/test/resources/PipeExtractorTest_resources/Struts2/arc/base/struts-2.3.30/output.pipe";
+        String oraclePath = pwd + oracleFile.replace("///", File.separator);
         
-        System.out.println(System.getProperty("user.dir"));
-
         // Call PipeExtractor.main() 
         // (arguments: sys version dir, output dir, selected language)
         assertDoesNotThrow( () -> {
-            PipeExtractor.main(new String[] {classesDir, resultDir, "java"});
+            PipeExtractor.main(new String[] {classesDir, resultDir, language});
         });
 
         // Read result instances into a set
-        InstanceList resultInstances = InstanceList.load(new File(resultDir + "output.pipe"));
+        InstanceList resultInstances = InstanceList.load(new File(resultDir + "output.pipe")); // StemmerPipe issue :(
         Set<Instance> result = new HashSet<>();
         for (Instance i : resultInstances) {
             result.add(i);
