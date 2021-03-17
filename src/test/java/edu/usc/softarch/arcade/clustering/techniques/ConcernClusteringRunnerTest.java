f9a3e4e4343d2870ae5cb228d45527da5c2be2ee
Gina Yang <ginayang@usc.edu>
2021-03-16 23:46:13 -0700

    some code to reach the serialVersionUID issue

--- src/test/java/edu/usc/softarch/arcade/clustering/techniques/ConcernClusteringRunnerTest.java
+++ src/test/java/edu/usc/softarch/arcade/clustering/techniques/ConcernClusteringRunnerTest.java
@@ -1,72 +1,84 @@
 package edu.usc.softarch.arcade.clustering.techniques;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import edu.usc.softarch.arcade.facts.driver.CSourceToDepsBuilder;
-import edu.usc.softarch.arcade.facts.driver.JavaSourceToDepsBuilder;
-import edu.usc.softarch.arcade.facts.driver.SourceToDepsBuilder;
+import edu.usc.softarch.arcade.clustering.FastFeatureVectors;
+
 
 public class ConcernClusteringRunnerTest {
-    // members to check: 
-        // public static FastClusterArchitecture fastClusters;
-        // protected static ArrayList<Cluster> clusters;
-        // protected static FastFeatureVectors fastFeatureVectors;
-    @ParameterizedTest
-    @CsvSource({
-        // Test parameters (5): 
-        // [directory with single sys version], 
-        // [subdirectory (of the previous argument) containing the binaries], 
-        // [desired output directory], 
-        // [system language],
-        // [desired name for dependencies rsf]
-        ".///src///test///resources///JavaSourceToDepsBuilderTest_resources///binaries///struts-2.3.30,"
-        + "lib_struts,"
-        + ".///target///test_results///ConcernClusteringRunnerTest,"
-        + "java,"
-        + "struts-2.3.30_ccr_test.rsf",
-    })
-    public void updateFastClustersAndSimMatrixToReflectMergedClusterTest(String versionDir, String inClassesDir, String outputDir, String language, String depsRsf){
-        char fs = File.separatorChar;
-        String outputPath = "." + fs + "target" + fs + "test_results" + fs + "ConcernClusteringRunnerTest";
-        (new File(outputPath)).mkdirs();
-        
-        String fullSrcDir = versionDir.replace("///", File.separator);
-        String outputDirName = outputDir.replace("///", File.separator);
-        
-        // Construct builder object
-        SourceToDepsBuilder builder = new JavaSourceToDepsBuilder();;
-        if (language.equals("c")){
-            builder = new CSourceToDepsBuilder();
-        }
-        else if (!language.equals("java")) { // fail test if not java or c
-            fail("invalid language given as test parameter: must be \"java\" or \"c\"");
-        }
+	// members to check: 
+		// public static FastClusterArchitecture fastClusters;
+		// protected static FastFeatureVectors fastFeatureVectors;
+	@ParameterizedTest
+	@CsvSource({
+		/*** Test parameters: ***/ 
+		// [directory with single system version's src files], 
+		// [where the pipe and mallet files are (one above base/)], 
+		// [location of serialized FastFeatureVectors], 
+		// [file with serialized FastFeatureVectors],
+		// [system language]
+		// ... (TBD)
 
-        // assertDoesNotThrow with builder? will figure this out later
-        try{
-            builder.build(fullSrcDir + File.separator + inClassesDir, outputDirName + File.separator + depsRsf);
-        } catch (IOException ioe){
-            ioe.printStackTrace();
-        }
-        // Construct a ConcernClusteringRunnerObject
-        ConcernClusteringRunner runner = new ConcernClusteringRunner(
-            builder.getFfVecs(), fullSrcDir, outputDirName + "/base", language);
-        
-        // Serialize runner.fastClusters, runner.clusters, runner.fastfeaturevectors
+		// // struts 2.3.30
+		// ".///src///test///resources///PipeExtractorTest_resources///src///struts-2.3.30,"
+		// + ".///src///test///resources///mallet_resources///struts-2.3.30,"
+		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources///serialized,"
+		// + "struts-2.3.30_output_ffVecs_before.txt,"
+		// + "java",
 
-    }
+		// // struts 2.5.2
+		// ".///src///test///resources///PipeExtractorTest_resources///src///struts-2.3.30,"
+		// + ".///src///test///resources///mallet_resources///struts-2.3.30,"
+		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources///serialized,"
+		// + "struts-2.5.2_output_ffVecs_before.txt,"
+		// + "java",
 
-    // TEST ALL METHODS THAT MODIFY FastClusterArchitecture fastClusters
-    // updateFastClustersAndSimMatrixToReflectMergedCluster (need to test) - called by computeClustersWithConcernsAndFastClusters 
-    // initializeDocTopicsForEachFastCluster --> lots of stuff happens here, test!
+		// // httpd-2.3.8
+		// ".///src///test///resources///PipeExtractorTest_resources///src///httpd-2.3.8,"
+		// + ".///src///test///resources///mallet_resources///httpd-2.3.8,"
+		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources///serialized,"
+		// + "httpd-2.3.8_output_ffVecs_before.txt,"
+		// + "java",
+
+		// httpd-2.4.26
+		".///src///test///resources///PipeExtractorTest_resources///src///httpd-2.4.26,"
+		+ ".///src///test///resources///mallet_resources///httpd-2.4.26,"
+		+ ".///src///test///resources///ConcernClusteringRunnerTest_resources///serialized,"
+		+ "httpd-2.4.26_output_ffVecs_before.txt,"
+		+ "java",
+	})
+	public void initializeDocTopicsForEachFastClusterTest(String srcDir, String outDir, String resDir, String ffvName, String language){
+		char fs = File.separatorChar;
+		String outputPath = "." + fs + "target" + fs + "test_results" + fs + "ConcernClusteringRunnerTest";
+		(new File(outputPath)).mkdirs();
+		
+		String fullSrcDir = srcDir.replace("///", File.separator);
+		String outputDir = outDir.replace("///", File.separator);
+		
+		// Deserialize FastFeatureVectors object
+		assertDoesNotThrow( () -> {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(resDir + fs + ffvName));
+			FastFeatureVectors oisffVecs = (FastFeatureVectors)ois.readObject();
+			ois.close();
+			// Construct a ConcernClusteringRunner object
+			ConcernClusteringRunner runner = new ConcernClusteringRunner(
+				oisffVecs, fullSrcDir, outputDir + "/base", language);
+		});
+
+	}
+
+	// TEST METHODS
+	// updateFastClustersAndSimMatrixToReflectMergedCluster - called by computeClustersWithConcernsAndFastClusters 
+	// initializeDocTopicsForEachFastCluster
 
 
 }
