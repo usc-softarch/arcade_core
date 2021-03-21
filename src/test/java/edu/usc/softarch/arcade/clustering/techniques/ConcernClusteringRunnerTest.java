909170aba8a73b391d1a4054b4ba18cf2f3ff1ee
Gina Yang <ginayang@usc.edu>
2021-03-21 12:07:56 -0700

    adding computeClustersWithConcernsAndFastClustersTest

--- src/test/java/edu/usc/softarch/arcade/clustering/techniques/ConcernClusteringRunnerTest.java
+++ src/test/java/edu/usc/softarch/arcade/clustering/techniques/ConcernClusteringRunnerTest.java
@@ -1,8 +1,12 @@
 package edu.usc.softarch.arcade.clustering.techniques;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
@@ -11,14 +15,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import edu.usc.softarch.arcade.clustering.FastClusterArchitecture;
 import edu.usc.softarch.arcade.clustering.FastFeatureVectors;
 
-// NOTE: run tests sequentially (need to figure out BeforeEach methods here later)
+// NOTE: run tests sequentially (need to figure out BeforeEach methods/TestInfo for parameter injection here later)
 public class ConcernClusteringRunnerTest {
 	ConcernClusteringRunner runner;
 	// members to check: 
@@ -49,10 +52,10 @@ public class ConcernClusteringRunnerTest {
 		// + "java",
 
 		// // httpd-2.3.8
-		// ".///src///test///resources///CSourceToDepsBuilderTest_resources///binaries///httpd-2.3.8,"
+		// ".///src///test///resources///CSourceToDepsBuilderTest_resources///src///httpd-2.3.8,"
 		// + ".///src///test///resources///mallet_resources///httpd-2.3.8,"
-		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources///serialized,"
-		// + "httpd-2.3.8_output_ffVecs_before.txt,"
+		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources,"
+		// + "httpd-2.3.8,"
 		// + "c",
 
 		// // httpd-2.4.26
@@ -62,8 +65,10 @@ public class ConcernClusteringRunnerTest {
 		// + "httpd-2.4.26_output_ffVecs_before.txt,"
 		// + "c",
 	})
-	public void initFastClustersTest(String srcDir, String outDir, String resDir, String versionName, String language){
+	public void initDataStructuresTest(String srcDir, String outDir, String resDir, String versionName, String language){
 		/* Checks that ConcernClusteringRunner.fastFeatureVectors is not null after the ConcernClusteringRunner constructor call */
+		/* Checks that ConcernClusteringRunner.fastClusters is modified in the ConcernClusteringRunner constructor */
+		// TODO: assertAll(), clean up the try/catch statements
 		char fs = File.separatorChar;
 		String outputPath = "." + fs + "target" + fs + "test_results" + fs + "ConcernClusteringRunnerTest";
 		(new File(outputPath)).mkdirs();
@@ -79,10 +84,13 @@ public class ConcernClusteringRunnerTest {
 			builderffVecs = (FastFeatureVectors)ois.readObject();
 			ois.close();
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -101,11 +109,131 @@ public class ConcernClusteringRunnerTest {
 			ois.close();
 			// Deserialize fastClusters from after initializeDocTopicsForEachFastCluster() call
 			ois = new ObjectInputStream(new FileInputStream(resDir + fs + "ds_serialized" + fs + versionName + "_fastClusters_after_init.txt"));
-			FastClusterArchitecture fastClustersAfter= (FastClusterArchitecture) ois.readObject();
-			System.out.println("clusters size before: " + fastClustersBefore.size());
-			System.out.println("clusters size after: " + fastClustersAfter.size());
-			assertNotEquals(fastClustersAfter, fastClustersBefore);
+			FastClusterArchitecture fastClustersAfterInit = (FastClusterArchitecture) ois.readObject();
+			// System.out.println("fastClusters size before: " + fastClustersBefore.size());
+			// System.out.println("fastClusters size after: " + fastClustersAfter.size());
+			ois.close();
 
+
+			assertFalse(fastClustersBefore.isEmpty()); // every node should get a cluster, so there should be at least one cluster?
+			assertAll( // Only executed if the previous assertion passes
+				() -> assertFalse(fastClustersAfterInit.isEmpty(), "fastClusters empty after initializeDocTopicsForEachFastCluster"),
+				() -> assertNotEquals(fastClustersAfterInit, fastClustersBefore)
+			);
+
+
+			// Deserialize fastFeatureVectors from after setFastFeatureVectors() call
+			ois = new ObjectInputStream(new FileInputStream(resDir + fs + "ds_serialized" + fs + versionName + "_fastfeatureVectors_init.txt"));
+			FastFeatureVectors ffvInit = (FastFeatureVectors) ois.readObject();
+			// Should not be null
+			assertNotNull(ffvInit);
+			ois.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		/*** Test parameters: ***/ 
+		// [directory with single system version's src files], 
+		// [path to the pipe and mallet files are (one directory above base/)], 
+		// [path to serialized objects (resources directory)],
+		// [system version]
+		// [system language]
+		// ... (TBD)
+
+		// struts 2.3.30
+		".///src///test///resources///PipeExtractorTest_resources///src///struts-2.3.30," // PLACE SRC FILES HERE
+		+ ".///src///test///resources///mallet_resources///struts-2.3.30,"
+		+ ".///src///test///resources///ConcernClusteringRunnerTest_resources,"
+		+ "struts-2.3.30,"
+		+ "java",
+
+		// // struts 2.5.2
+		// ".///src///test///resources///PipeExtractorTest_resources///src///struts-2.3.30," // PLACE SRC FILES HERE
+		// + ".///src///test///resources///mallet_resources///struts-2.3.30,"
+		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources///serialized,"
+		// + "struts-2.5.2_output_ffVecs_before.txt,"
+		// + "java",
+
+		// // httpd-2.3.8
+		// ".///src///test///resources///CSourceToDepsBuilderTest_resources///src///httpd-2.3.8,"
+		// + ".///src///test///resources///mallet_resources///httpd-2.3.8,"
+		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources,"
+		// + "httpd-2.3.8,"
+		// + "c",
+
+		// // httpd-2.4.26
+		// ".///src///test///resources///CSourceToDepsBuilderTest_resources///binaries///httpd-2.4.26,"
+		// + ".///src///test///resources///mallet_resources///httpd-2.4.26,"
+		// + ".///src///test///resources///ConcernClusteringRunnerTest_resources///serialized,"
+		// + "httpd-2.4.26_output_ffVecs_before.txt,"
+		// + "c",
+	})
+	public void computeClustersWithConcernsAndFastClustersTest(String srcDir, String outDir, String resDir, String versionName, String language){
+		// This function updates fastClusters in its call to updateFastClustersAndSimMatrixToReflectMergedCluster()
+		// is computeClustersWithConcernsAndFastClusters ever called with stopCriterion = "clustergain"? (I don't think this parameter affects the fastClusters anyway)
+
+		char fs = File.separatorChar;
+		String outputPath = "." + fs + "target" + fs + "test_results" + fs + "ConcernClusteringRunnerTest";
+		(new File(outputPath)).mkdirs();
+		
+		String fullSrcDir = srcDir.replace("///", File.separator);
+		String outputDir = outDir.replace("///", File.separator);
+		
+		// Deserialize FastFeatureVectors object
+		ObjectInputStream ois;
+		FastFeatureVectors builderffVecs = null;
+		try {
+			ois = new ObjectInputStream(new FileInputStream(resDir + fs + "ffVecs_serialized" + fs + versionName + "_ffVecs_builder.txt"));
+			builderffVecs = (FastFeatureVectors)ois.readObject();
+			ois.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (builderffVecs == null){
+			fail("failed to deserialize FastFeatureVectors from builder object");
+		}
+		
+		// Construct a ConcernClusteringRunner object
+		runner = new ConcernClusteringRunner(
+			builderffVecs, fullSrcDir, outputDir + "/base", language);
+		// call computeClustersWithConcernsAndFastClusters()
+		assertDoesNotThrow(() -> {
+			int numClusters = (int) ((double) runner.getFastClusters().size() * .20); // copied from BatchClusteringEngine
+			runner.computeClustersWithConcernsAndFastClusters(new ConcernClusteringRunner.PreSelectedStoppingCriterion(numClusters), "preselected", "js"); // copied from BatchClusteringEngine
+		});
+
+		// check fastClusters
+		try {
+			// Deserialize initial fastClusters
+			ois = new ObjectInputStream(new FileInputStream(resDir + fs + "ds_serialized" + fs + versionName + "_fastClusters_after_init.txt"));
+			FastClusterArchitecture fastClustersAfterInit = (FastClusterArchitecture) ois.readObject();
+			ois.close();
+			// Deserialize fastClusters from after computeClustersWithConcernsAndFastClusters() call
+			ois = new ObjectInputStream(new FileInputStream(resDir + fs + "ds_serialized" + fs + versionName + "_fastClusters_after_compute.txt"));
+			FastClusterArchitecture fastClustersCompute = (FastClusterArchitecture) ois.readObject();
+			ois.close();
+			assertFalse(fastClustersCompute.isEmpty());
+			assertTrue(fastClustersCompute.size() < fastClustersAfterInit.size()); // size should be less than?
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -119,14 +247,10 @@ public class ConcernClusteringRunnerTest {
 		
 	}
 
-	
 
 	// TEST METHODS
-	// updateFastClustersAndSimMatrixToReflectMergedCluster - called by computeClustersWithConcernsAndFastClusters 
+	// computeClustersWithConcernsAndFastClusters 
 	// mergeFastClustersUsingTopics
-
-	// updateFastClustersAndSimMatrixToReflectMergedCluster
 	// identifyMostSimClusters
-	// computeClustersWithConcernsAndFastClusters -> computeClusters
 
 }
