6b3213d7bb15607d47c756357bf63346890aed3b
Gina Yang <ginayang@usc.edu>
2021-03-27 16:33:25 -0700

    arc setup script fixes; add script for linux

--- src/test/resources/arc_setup.sh
+++ src/test/resources/arc_setup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 
 mvn -Dtest=edu.usc.softarch.arcade.util.ldasupport.PipeExtractorTest test
+
 ./ext-tools/mallet-2.0.7/bin/mallet.bat import-dir --input subject_systems/Struts2/src/struts-2.3.30 --remove-stopwords TRUE --keep-sequence TRUE --output target/test_results/BatchClusteringEngineTest/struts-2.3.30/base/topicmodel.data
 
 ./ext-tools/mallet-2.0.7/bin/mallet.bat train-topics --input target/test_results/BatchClusteringEngineTest/struts-2.3.30/base/topicmodel.data --inferencer-filename target/test_results/BatchClusteringEngineTest/struts-2.3.30/base/infer.mallet --num-top-words 50 --num-topics 100 --num-threads 3 --num-iterations 100 --doc-topics-threshold 0.1
