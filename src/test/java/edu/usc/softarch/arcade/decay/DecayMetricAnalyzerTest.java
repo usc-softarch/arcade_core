ed6c2f90ba9cec90b3178668afcbb67586ebd757
MarceloLaser <marcelo.laser@gmail.com>
2021-04-07 21:08:05 -0700

    Minor refactorings to decay metrics tests

--- src/test/java/edu/usc/softarch/arcade/decay/DecayMetricAnalyzerTest.java
+++ src/test/java/edu/usc/softarch/arcade/decay/DecayMetricAnalyzerTest.java
@@ -25,150 +25,139 @@ import edu.usc.softarch.arcade.util.FileListing;
 import edu.usc.softarch.arcade.util.FileUtil;
 
 public class DecayMetricAnalyzerTest {
-    String resourcesDir = ".///src///test///resources///decay_metrics_resources";
+	String resourcesDir = ".///src///test///resources///decay_metrics_resources";
 
-    @ParameterizedTest
-    @CsvSource ({
-        // httpd
-        "///httpd///acdc_cluster,"
-        + "///httpd///acdc_dep,"
-        + "///httpd///oracles///decay_metrics_oracle_httpd_acdc.txt",
-        
-        // struts
-        "///struts///acdc_cluster,"
-        + "///struts///acdc_dep,"
-        + "///struts///oracles///decay_metrics_oracle_struts_acdc.txt",
-    })
-    public void mainTest(String clusters, String deps, String oracle){
-        String resDir = FileUtil.tildeExpandPath(resourcesDir.replace("///", File.separator));
-        String depsDir = FileUtil.tildeExpandPath(resDir + deps.replace("///", File.separator));
-        String clustersDir = FileUtil.tildeExpandPath((resDir + clusters.replace("///", File.separator)));
-        
-
-        List<File> clusterFiles = assertDoesNotThrow( () -> {
-            return FileListing.getFileListing(new File(clustersDir));
-        });
-		List<File> depsFiles = assertDoesNotThrow( () -> {
-            return FileListing.getFileListing(new File(depsDir));
-        });
-		
+	@ParameterizedTest
+	@CsvSource ({
+			// httpd
+			"///httpd///acdc_cluster,"
+			+ "///httpd///acdc_dep,"
+			+ "///httpd///oracles///decay_metrics_oracle_httpd_acdc.txt",
+			
+			// struts
+			"///struts///acdc_cluster,"
+			+ "///struts///acdc_dep,"
+			+ "///struts///oracles///decay_metrics_oracle_struts_acdc.txt",
+	})
+	public void mainTest(String clusters, String deps, String oracle) {
+		// Setup
+		String resDir = FileUtil.tildeExpandPath(resourcesDir.replace("///", File.separator));
+		String depsDir = FileUtil.tildeExpandPath(resDir + deps.replace("///", File.separator));
+		String clustersDir = FileUtil.tildeExpandPath((resDir + clusters.replace("///", File.separator)));
+			
+		List<File> clusterFiles = assertDoesNotThrow(() -> {
+			return FileListing.getFileListing(new File(clustersDir));
+		});
+		List<File> depsFiles = assertDoesNotThrow(() -> {
+			return FileListing.getFileListing(new File(depsDir));
+		});
+	
 		clusterFiles = FileUtil.sortFileListByVersion(clusterFiles);
-		
 		Map<String,List<Double>> decayMetrics = new LinkedHashMap<>();
-
 		String versionSchemeExpr = "[0-9]+\\.[0-9]+(\\.[0-9]+)*";
+
 		for (File clusterFile : clusterFiles) {
-			if (clusterFile.getName().endsWith(".rsf")) {
-				String clusterVersion = FileUtil.extractVersionFromFilename(versionSchemeExpr, clusterFile.getName());
-				
+			if (!clusterFile.getName().endsWith(".rsf")) continue;
+			String clusterVersion = FileUtil.extractVersionFromFilename(versionSchemeExpr, clusterFile.getName());
+			
+			for (File depsFile : depsFiles) {
+				if (!depsFile.getName().endsWith(".rsf"))	continue;
+				String depsVersion = FileUtil.extractVersionFromFilename(versionSchemeExpr, depsFile.getName());
+
 				// Identify appropriate deps file version
-				for (File depsFile : depsFiles) {
-					if (depsFile.getName().endsWith(".rsf")) {
-						String depsVersion = FileUtil.extractVersionFromFilename(versionSchemeExpr, depsFile.getName());
-						if (clusterVersion.equals(depsVersion)) {
-							String[] dmaArgs = {clusterFile.getAbsolutePath(),depsFile.getAbsolutePath()};
-							DecayMetricAnalyzer.main(dmaArgs);
-							
-							List<Double> rciVals = null; 
-							if (decayMetrics.get("rci") != null) {
-								rciVals = decayMetrics.get("rci");
-							}
-							else {
-								rciVals = new ArrayList<>();
-							}
-							rciVals.add(DecayMetricAnalyzer.rciVal);
-							decayMetrics.put("rci", rciVals);
-							
-							List<Double> twoWayRatios = null;
-							if (decayMetrics.get("twoway") != null) {
-								twoWayRatios = decayMetrics.get("twoway");
-							}
-							else {
-								twoWayRatios = new ArrayList<>();
-							}
-							twoWayRatios.add(DecayMetricAnalyzer.twoWayPairRatio);
-							decayMetrics.put("twoway", twoWayRatios);
-							
-							List<Double> stabilityVals = null;
-							if (decayMetrics.get("stability") != null) {
-								stabilityVals = decayMetrics.get("stability");
-							}
-							else {
-								stabilityVals = new ArrayList<>();
-							}
-							stabilityVals.add(DecayMetricAnalyzer.avgStability);
-							decayMetrics.put("stability", stabilityVals);
-							
-							List<Double> mqRatios = null;
-							if (decayMetrics.get("mq") != null) {
-								mqRatios = decayMetrics.get("mq");
-							}
-							else {
-								mqRatios = new ArrayList<>();
-							}
-							mqRatios.add(DecayMetricAnalyzer.mqRatio);
-							decayMetrics.put("mq", mqRatios);
-							
-							break;
-						}
+				if (clusterVersion.equals(depsVersion)) {
+					String[] dmaArgs = { clusterFile.getAbsolutePath(), depsFile.getAbsolutePath() };
+					DecayMetricAnalyzer.main(dmaArgs);
+					
+					List<Double> rciVals = null;
+					if (decayMetrics.get("rci") != null) {
+						rciVals = decayMetrics.get("rci");
+					}	else {
+						rciVals = new ArrayList<>();
 					}
+					rciVals.add(DecayMetricAnalyzer.rciVal);
+					decayMetrics.put("rci", rciVals);
+					
+					List<Double> twoWayRatios = null;
+					if (decayMetrics.get("twoway") != null) {
+						twoWayRatios = decayMetrics.get("twoway");
+					}	else {
+						twoWayRatios = new ArrayList<>();
+					}
+					twoWayRatios.add(DecayMetricAnalyzer.twoWayPairRatio);
+					decayMetrics.put("twoway", twoWayRatios);
+					
+					List<Double> stabilityVals = null;
+					if (decayMetrics.get("stability") != null) {
+						stabilityVals = decayMetrics.get("stability");
+					}	else {
+						stabilityVals = new ArrayList<>();
+					}
+					stabilityVals.add(DecayMetricAnalyzer.avgStability);
+					decayMetrics.put("stability", stabilityVals);
+					
+					List<Double> mqRatios = null;
+					if (decayMetrics.get("mq") != null) {
+						mqRatios = decayMetrics.get("mq");
+					}	else {
+						mqRatios = new ArrayList<>();
+					}
+					mqRatios.add(DecayMetricAnalyzer.mqRatio);
+					decayMetrics.put("mq", mqRatios);
+					
+					break;
 				}
 			}
 		}
 
-        // construct the DescriptiveStatistics since we can't get kurtosis or skewness otherwise
-        Map<String,Map<String,Double>> allStats = new LinkedHashMap<>();
-        for (String key : decayMetrics.keySet()) {
+		// construct the DescriptiveStatistics since we can't get kurtosis or skewness otherwise
+		Map<String, Map<String, Double>> allStats = new LinkedHashMap<>();
+		for (String key : decayMetrics.keySet()) {
 			List<Double> vals = decayMetrics.get(key);
 			double[] valArr = ArrayUtils.toPrimitive(vals.toArray(new Double[vals.size()]));
 			Map<String, Double> rawStats = new LinkedHashMap<>();
-            DescriptiveStatistics stats = new DescriptiveStatistics(valArr);
+			DescriptiveStatistics stats = new DescriptiveStatistics(valArr);
 
-            rawStats.put("n", (double) (stats.getN()));
-            rawStats.put("min", stats.getMin());
-            rawStats.put("max", stats.getMax());
-            rawStats.put("mean", stats.getMean());
-            rawStats.put("std dev", stats.getStandardDeviation());
-            rawStats.put("median", stats.getPercentile(50));
-            rawStats.put("skewness",stats.getSkewness());
+			rawStats.put("n", (double) (stats.getN()));
+			rawStats.put("min", stats.getMin());
+			rawStats.put("max", stats.getMax());
+			rawStats.put("mean", stats.getMean());
+			rawStats.put("std dev", stats.getStandardDeviation());
+			rawStats.put("median", stats.getPercentile(50));
+			rawStats.put("skewness",stats.getSkewness());
 			rawStats.put("kurtosis",stats.getKurtosis());
 
-            allStats.put(key, rawStats);
+			allStats.put(key, rawStats);
 		}
 
-        // parse oracle
-        List<List<String>> records = new ArrayList<>();
-        String oraclePath = resourcesDir + oracle;
-        oraclePath.replace("///", File.separator);
-        try{
-            BufferedReader br = assertDoesNotThrow( () -> {
-                return new BufferedReader(new FileReader(oraclePath));
-            });
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
+		// parse oracle
+		List<List<String>> records = new ArrayList<>();
+		String oraclePath = resourcesDir + oracle;
+		oraclePath.replace("///", File.separator);
+		try (BufferedReader br = new BufferedReader(new FileReader(oraclePath))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] values = line.split(",");
+				records.add(Arrays.asList(values));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Exception caught in DecayMetricAnalyzerTest mainTest");
+		}
 
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            fail("Exception caught in DecayMetricAnalyzerTest mainTest");
-        }
-
-		Map<String,Map<String, Double>> oracleStats = new LinkedHashMap<>();
-        for(int i = 0; i < records.size(); i++){
+		Map<String, Map<String, Double>> oracleStats = new LinkedHashMap<>();
+		for (int i = 0; i < records.size(); i++) {
 			System.out.println(records.get(i).size());
 			
 			// first entry in each line is the key
 			String oracleKey = records.get(i).get(0);
 			Map<String,Double> oracleRawStats = new LinkedHashMap<>();
 
-            for(int j = 1; j < records.get(i).size(); j+=2){
+			for(int j = 1; j < records.get(i).size(); j+=2){
 				oracleRawStats.put(records.get(i).get(j), Double.parseDouble(records.get(i).get(j+1)));
-            }
+			}
 			oracleStats.put(oracleKey, oracleRawStats);
-        }
+		}
 
 		// check rci values
 		Map<String, Double> rciVals = allStats.get("rci");
@@ -182,9 +171,9 @@ public class DecayMetricAnalyzerTest {
 			() -> assertEquals(rciVals.get("median"), rciOracles.get("median")),
 			() -> assertEquals(rciVals.get("skewness"), rciOracles.get("skewness")),
 			() -> assertEquals(rciVals.get("kurtosis"), rciOracles.get("kurtosis"))
-     	);
+		);
 
-	  	// check twoway values
+			// check twoway values
 		Map<String, Double> twowayVals = allStats.get("twoway");
 		Map<String, Double> twowayOracles = oracleStats.get("twoway");
 		assertAll(
@@ -196,7 +185,7 @@ public class DecayMetricAnalyzerTest {
 			() -> assertEquals(twowayVals.get("median"), twowayOracles.get("median")),
 			() -> assertEquals(twowayVals.get("skewness"), twowayOracles.get("skewness")),
 			() -> assertEquals(twowayVals.get("kurtosis"), twowayOracles.get("kurtosis"))
-      	);
+		);
 
 		// check stability values
 		Map<String, Double> stabilityVals = allStats.get("stability");
@@ -210,7 +199,7 @@ public class DecayMetricAnalyzerTest {
 			() -> assertEquals(stabilityVals.get("median"), stabilityOracles.get("median")),
 			() -> assertEquals(stabilityVals.get("skewness"), stabilityOracles.get("skewness")),
 			() -> assertEquals(stabilityVals.get("kurtosis"), stabilityOracles.get("kurtosis"))
-      	);
+		);
 
 		// check mq values
 		Map<String, Double> mqVals = allStats.get("mq");
@@ -224,6 +213,6 @@ public class DecayMetricAnalyzerTest {
 			() -> assertEquals(mqVals.get("median"), mqOracles.get("median")),
 			() -> assertEquals(mqVals.get("skewness"), mqOracles.get("skewness")),
 			() -> assertEquals(mqVals.get("kurtosis"), mqOracles.get("kurtosis"))
-      	);
-    }
+		);
+	}
 }
