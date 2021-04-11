bce63df7f9cb7bbfcaf4f7cccd366cb8e4529f6c
Tsun Ning Wong <tsunninw@usc.edu>
2021-04-10 22:06:05 -0700

    Fixed BatchSystemEvoTest, fixed oracles
    Test passes

--- src/test/java/edu/usc/softarch/arcade/metrics/BatchSystemEvoTest.java
+++ src/test/java/edu/usc/softarch/arcade/metrics/BatchSystemEvoTest.java
@@ -5,9 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.HashMap;
+import java.util.Scanner;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.junit.jupiter.api.BeforeEach;
@@ -35,19 +37,19 @@ public class BatchSystemEvoTest {
 		// [Statistics output path]
 
 		// ACDC Struts2 
-		".///src///test///resources///System_evo_resources///outputs_acdc///statistics_acdc_Struts2.txt,"
+		".///src///test///resources///System_evo_resources///outputs_acdc///Struts2_oracle.csv,"
 		+ ".///src///test///resources///System_evo_resources///cluster_files_acdc///Struts2",
 
 		// ACDC httpd
-		".///src///test///resources///System_evo_resources///outputs_acdc///statistics_acdc_httpd.txt,"
+		".///src///test///resources///System_evo_resources///outputs_acdc///httpd_oracle.csv,"
 		+ ".///src///test///resources///System_evo_resources///cluster_files_acdc///httpd",
 
 		// ARC Struts2
-		".///src///test///resources///System_evo_resources///outputs_arc///statistics_arc_Struts2.txt,"
+		".///src///test///resources///System_evo_resources///outputs_arc///Struts2_oracle.csv,"
 		+ ".///src///test///resources///System_evo_resources///cluster_files_arc///Struts2",
 
 		// ARC httpd
-		".///src///test///resources///System_evo_resources///outputs_arc///statistics_arc_httpd.txt,"
+		".///src///test///resources///System_evo_resources///outputs_arc///httpd_oracle.csv,"
 		+ ".///src///test///resources///System_evo_resources///cluster_files_arc///httpd",
 	})
 	public void batchSystemEvoTest(String oracle_path, String clusters_dir){
@@ -57,25 +59,42 @@ public class BatchSystemEvoTest {
 
 		assertDoesNotThrow(() -> BatchSystemEvo.main(args));
 
+		
+
 		try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(oraclePath));
-			DescriptiveStatistics oracle_stats = (DescriptiveStatistics) ois.readObject();
-			ois.close();
-			
+			HashMap<String, Double> oracle_map = new HashMap<String, Double>();
+			Scanner sc = new Scanner(new File(oraclePath));  
 			DescriptiveStatistics output_stats = BatchSystemEvo.getStats();
+			while (sc.hasNext()){
+				String line = sc.nextLine();
+				String arr[] = line.split(":");
+				oracle_map.put(arr[0],Double.parseDouble(arr[1]));
+			}
 			
+			/*
+			Round both the oracle and the output to 10 sig figs and compare.
+			Otherwise tests will not pass because the oracle and the output has different
+			number of significant figures
+			*/
 			assertAll(
-				() -> assertEquals(oracle_stats.getN(), output_stats.getN(),"N does not match the oracle"),
-				() -> assertEquals(oracle_stats.getN(), output_stats.getN(),"N does not match the oracle"),
-				() -> assertEquals(oracle_stats.getMin(), output_stats.getMin(),"Min does not match the oracle"),
-				() -> assertEquals(oracle_stats.getMax(), output_stats.getMax(),"Max does not match the oracle"),
-				() -> assertEquals(oracle_stats.getMean(), output_stats.getMean(),"Mean does not match the oracle"),
-				() -> assertEquals(oracle_stats.getStandardDeviation(), output_stats.getStandardDeviation(),"StandardDeviation does not match the oracle"),
-				() -> assertEquals(oracle_stats.getPercentile(50), output_stats.getPercentile(50),"Median does not match the oracle"),
-				() -> assertEquals(oracle_stats.getSkewness(), output_stats.getSkewness(),"Skewness does not match the oracle"),
-				() -> assertEquals(oracle_stats.getKurtosis(), output_stats.getKurtosis(),"Kurtosis does not match the oracle")
+				() -> assertEquals(oracle_map.get("n"), (double)(output_stats.getN()),"N does not match the oracle"),
+				() -> assertEquals(new BigDecimal(oracle_map.get("min")).round(new MathContext(10)).doubleValue(), 
+					new BigDecimal(output_stats.getMin()).round(new MathContext(10)).doubleValue(),"Min does not match the oracle"),
+				() -> assertEquals(new BigDecimal(oracle_map.get("max")).round(new MathContext(10)).doubleValue(), 
+					new BigDecimal(output_stats.getMax()).round(new MathContext(10)).doubleValue(),"Max does not match the oracle"),
+				() -> assertEquals(new BigDecimal(oracle_map.get("mean")).round(new MathContext(10)).doubleValue(), 
+					new BigDecimal(output_stats.getMean()).round(new MathContext(10)).doubleValue(),"Mean does not match the oracle"),
+				() -> assertEquals(new BigDecimal(oracle_map.get("std dev")).round(new MathContext(10)).doubleValue(), 
+					new BigDecimal(output_stats.getStandardDeviation()).round(new MathContext(10)).doubleValue(),"StandardDeviation does not match the oracle"),
+				() -> assertEquals(new BigDecimal(oracle_map.get("median")).round(new MathContext(10)).doubleValue(), 
+					new BigDecimal(output_stats.getPercentile(50)).round(new MathContext(10)).doubleValue(),"Median does not match the oracle"),
+				() -> assertEquals(new BigDecimal(oracle_map.get("skewness")).round(new MathContext(10)).doubleValue(), 
+					new BigDecimal(output_stats.getSkewness()).round(new MathContext(10)).doubleValue(),"Skewness does not match the oracle"),
+				() -> assertEquals(new BigDecimal(oracle_map.get("kurtosis")).round(new MathContext(10)).doubleValue(), 
+					new BigDecimal(output_stats.getKurtosis()).round(new MathContext(10)).doubleValue(),"Kurtosis does not match the oracle")
 				);	
-		} catch(IOException | ClassNotFoundException e) {
+			
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
