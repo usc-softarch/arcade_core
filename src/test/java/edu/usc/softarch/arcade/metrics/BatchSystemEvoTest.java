d85c4dbace256521933a97c2ce1a873a45cc34b2
Tsun Ning Wong <tsunninw@usc.edu>
2021-04-10 23:28:42 -0700

    Fixed test and oracle

--- src/test/java/edu/usc/softarch/arcade/metrics/BatchSystemEvoTest.java
+++ src/test/java/edu/usc/softarch/arcade/metrics/BatchSystemEvoTest.java
@@ -4,12 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.MathContext;
 import java.util.HashMap;
-import java.util.Scanner;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.junit.jupiter.api.BeforeEach;
@@ -63,35 +62,24 @@ public class BatchSystemEvoTest {
 
 		try {
 			HashMap<String, Double> oracle_map = new HashMap<String, Double>();
-			Scanner sc = new Scanner(new File(oraclePath));  
+			BufferedReader br = new BufferedReader(new FileReader(oraclePath));  
 			DescriptiveStatistics output_stats = BatchSystemEvo.getStats();
-			while (sc.hasNext()){
-				String line = sc.nextLine();
+			String line;
+			while ((line = br.readLine()) != null){
 				String arr[] = line.split(":");
 				oracle_map.put(arr[0],Double.parseDouble(arr[1]));
 			}
-			
-			/*
-			Round both the oracle and the output to 10 sig figs and compare.
-			Otherwise tests will not pass because the oracle and the output has different
-			number of significant figures
-			*/
+			br.close();
+
 			assertAll(
 				() -> assertEquals(oracle_map.get("n"), (double)(output_stats.getN()),"N does not match the oracle"),
-				() -> assertEquals(new BigDecimal(oracle_map.get("min")).round(new MathContext(10)).doubleValue(), 
-					new BigDecimal(output_stats.getMin()).round(new MathContext(10)).doubleValue(),"Min does not match the oracle"),
-				() -> assertEquals(new BigDecimal(oracle_map.get("max")).round(new MathContext(10)).doubleValue(), 
-					new BigDecimal(output_stats.getMax()).round(new MathContext(10)).doubleValue(),"Max does not match the oracle"),
-				() -> assertEquals(new BigDecimal(oracle_map.get("mean")).round(new MathContext(10)).doubleValue(), 
-					new BigDecimal(output_stats.getMean()).round(new MathContext(10)).doubleValue(),"Mean does not match the oracle"),
-				() -> assertEquals(new BigDecimal(oracle_map.get("std dev")).round(new MathContext(10)).doubleValue(), 
-					new BigDecimal(output_stats.getStandardDeviation()).round(new MathContext(10)).doubleValue(),"StandardDeviation does not match the oracle"),
-				() -> assertEquals(new BigDecimal(oracle_map.get("median")).round(new MathContext(10)).doubleValue(), 
-					new BigDecimal(output_stats.getPercentile(50)).round(new MathContext(10)).doubleValue(),"Median does not match the oracle"),
-				() -> assertEquals(new BigDecimal(oracle_map.get("skewness")).round(new MathContext(10)).doubleValue(), 
-					new BigDecimal(output_stats.getSkewness()).round(new MathContext(10)).doubleValue(),"Skewness does not match the oracle"),
-				() -> assertEquals(new BigDecimal(oracle_map.get("kurtosis")).round(new MathContext(10)).doubleValue(), 
-					new BigDecimal(output_stats.getKurtosis()).round(new MathContext(10)).doubleValue(),"Kurtosis does not match the oracle")
+				() -> assertEquals(oracle_map.get("min"), output_stats.getMin(),"Min does not match the oracle"),
+				() -> assertEquals(oracle_map.get("max"), output_stats.getMax(),"Max does not match the oracle"),
+				() -> assertEquals(oracle_map.get("mean"), output_stats.getMean(),"Mean does not match the oracle"),
+				() -> assertEquals(oracle_map.get("std dev"), output_stats.getStandardDeviation(),"StandardDeviation does not match the oracle"),
+				() -> assertEquals(oracle_map.get("median"), output_stats.getPercentile(50),"Median does not match the oracle"),
+				() -> assertEquals(oracle_map.get("skewness"), output_stats.getSkewness(),"Skewness does not match the oracle"),
+				() -> assertEquals(oracle_map.get("kurtosis"), output_stats.getKurtosis(),"Kurtosis does not match the oracle")
 				);	
 			
 		} catch(IOException e) {
