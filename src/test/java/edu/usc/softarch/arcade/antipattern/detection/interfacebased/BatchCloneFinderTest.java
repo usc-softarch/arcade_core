729e3b10691de6af61ea13fb19203262817a8d98
MarceloLaser <marcelo.laser@gmail.com>
2021-10-24 00:04:04 -0700

    Formatting and documentation of BatchCloneFinderTest

--- src/test/java/edu/usc/softarch/arcade/antipattern/detection/interfacebased/BatchCloneFinderTest.java
+++ src/test/java/edu/usc/softarch/arcade/antipattern/detection/interfacebased/BatchCloneFinderTest.java
@@ -15,77 +15,114 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import edu.usc.softarch.arcade.util.FileUtil;
 
+/**
+ * Tests whether the results of PMD are correct for what ARCADE expects.
+ */
 public class BatchCloneFinderTest {
 	private char fs = File.separatorChar;
 
+	/**
+	 * Sets up the CLI command to run PMD.
+	 * 
+	 * @param version The name and version of the sample system.
+	 * @return The command and arguments needed to run PMD.
+	 */
 	private List<String> buildArguments(String version) {
 		List<String> command = new ArrayList<>();
-		command.add("ext-tools" + fs + "apache-ant-1.9.6"  + fs + "bin" 
+
+		// The actual system being run is ANT. PMD is run through it.
+		command.add("ext-tools" + fs + "apache-ant-1.9.6"  + fs + "bin"
 			+ fs + "ant.bat");
+		// -f is to input a file
 		command.add("-f");
+		// The file being input is to run PMD
 		command.add("ext-tools" + fs + "pmd-bin-5.3.2"  + fs + "cpd.xml");
+		// CPD is the copy-paste detector of PMD
 		command.add("cpd");
+		// This is the input for PMD
 		command.add("-Din=" + System.getProperty("user.dir") + fs + "src" + fs
 			+ "test" + fs + "resources" + fs + "subject_systems_resources" + fs
 			+ "nutch" + fs + "src" + fs + version);
+		// This is the path for an output file.
 		command.add("-Dout="+ System.getProperty("user.dir") + fs + "target" + fs
 			+ "test_results" + fs + "BatchCloneFinderTest" + fs + version 
 			+ "_clone.xml");
+
 		return command;
 	}
 
+	/**
+	 * Sets up and runs the Process object responsible for PMD.
+	 * 
+	 * @param version The name and version of the sample system.
+	 */
 	public void setUp(String version) {
 		(new File("target" + fs + "test_results" + fs + "BatchCloneFinderTest"))
 			.mkdirs();
 		List<String> command = buildArguments(version);
 		ProcessBuilder pb = new ProcessBuilder(command);
 		pb.inheritIO();
+
 		try {
 			Process p = pb.start();
 			p.waitFor();
 		} catch(IOException ioe) {
 			ioe.printStackTrace();
-			fail("failed to start Process");
+			fail("Failed to start Process.");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-			fail("failed to wait for Process");
+			fail("Failed to wait for Process.");
 		}
 	}
 	
+	/**
+	 * Test for PMD's CPD module, used as a clone detector in ARCADE. The goal of
+	 * this test is to ensure that the output of PMD is what is expected by
+	 * ARCADE, both as a sanity check and as a gatekeeping mechanism if we ever
+	 * opt to use a newer version of PMD, or a different clone detector.
+	 * 
+	 * @param version The name and version of the sample system.
+	 * @param oracle The path to the test oracle file.
+	 */
 	@ParameterizedTest
 	@CsvSource({
-		// // struts-2.3.30
-		// "struts-2.3.30,"
-		// + ".///src///test///resources///BatchCloneFinderTest_resources///struts-2.3.30_clone.xml",
-		// // struts-2.5.2
-		// "struts-2.5.2,"
-		// + ".///src///test///resources///BatchCloneFinderTest_resources///struts-2.5.2_clone.xml",
+		// struts-2.3.30
+		"struts-2.3.30,"
+		+ ".///src///test///resources///BatchCloneFinderTest_resources"
+			+ "///struts-2.3.30_clone.xml",
+		// struts-2.5.2
+		"struts-2.5.2,"
+		+ ".///src///test///resources///BatchCloneFinderTest_resources"
+			+ "///struts-2.5.2_clone.xml",
 		// nutch-1.7
 		"nutch-1.7,"
-		+ ".///src///test///resources///BatchCloneFinderTest_resources///nutch-1.7_clone.xml",
+		+ ".///src///test///resources///BatchCloneFinderTest_resources"
+			+ "///nutch-1.7_clone.xml",
 		// nutch-1.8
 		"nutch-1.8,"
-		+ ".///src///test///resources///BatchCloneFinderTest_resources///nutch-1.8_clone.xml",
+		+ ".///src///test///resources///BatchCloneFinderTest_resources"
+			+ "///nutch-1.8_clone.xml",
 		// nutch-1.8
 		"nutch-1.9,"
-		+ ".///src///test///resources///BatchCloneFinderTest_resources///nutch-1.9_clone.xml",
+		+ ".///src///test///resources///BatchCloneFinderTest_resources"
+			+ "///nutch-1.9_clone.xml",
 	})
 	public void singleTest(String version, String oracle) {
-		//TODO Need to fix the absolute paths in the oracle from this, so the comparison works
-		// // Constructs ProcessBuilder
-		// setUp(version);
+		// Constructs ProcessBuilder
+		setUp(version);
 
-		// String oraclePath = oracle.replace("///", File.separator);
-		// String resultPath = "target" + fs + "test_results" + fs + "BatchCloneFinderTest" + fs + version + "_clone.xml";
+		String oraclePath = oracle.replace("///", File.separator);
+		String resultPath = "target" + fs + "test_results" + fs
+			+ "BatchCloneFinderTest" + fs + version + "_clone.xml";
 
-		// // Read in xmls as Strings
-		// String resultString = assertDoesNotThrow( () -> {
-		//   return FileUtil.readFile(resultPath, StandardCharsets.UTF_8);
-		// });
-		// String oracleString = assertDoesNotThrow( () -> {
-		//   return FileUtil.readFile(oraclePath, StandardCharsets.UTF_8);
-		// });
+		// Read in xmls as Strings
+		String resultString = assertDoesNotThrow(() -> {
+		  return FileUtil.readFile(resultPath, StandardCharsets.UTF_8);
+		});
+		String oracleString = assertDoesNotThrow(() -> {
+		  return FileUtil.readFile(oraclePath, StandardCharsets.UTF_8);
+		});
 
-		// assertEquals(oracleString, resultString);
+		assertEquals(oracleString, resultString);
 	}
 }
