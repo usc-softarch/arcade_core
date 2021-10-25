4d8ffc5744d867e076344181c63fa3909c7186fa
MarceloLaser <marcelo.laser@gmail.com>
2021-10-24 20:41:54 -0700

    changed asdWithConcerns to call ASD directly

--- src/test/java/edu/usc/softarch/arcade/clustering/drivers/BatchClusteringEngineTest.java
+++ src/test/java/edu/usc/softarch/arcade/clustering/drivers/BatchClusteringEngineTest.java
@@ -11,7 +11,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import edu.usc.softarch.arcade.antipattern.SmellCollection;
+import edu.usc.softarch.arcade.antipattern.detection.ArchSmellDetector;
 import edu.usc.softarch.arcade.clustering.techniques.ConcernClusteringRunner;
+import edu.usc.softarch.arcade.topics.DocTopics;
+import edu.usc.softarch.arcade.topics.TopicModelExtractionMethod;
+import edu.usc.softarch.arcade.topics.TopicUtil;
 import edu.usc.softarch.arcade.util.FileUtil;
 import edu.usc.softarch.arcade.util.RsfCompare;
 
@@ -114,54 +118,62 @@ public class BatchClusteringEngineTest {
 		// [expected clusters file name]
 
 		// struts 2.3.30
-		".///src///test///resources///subject_systems_resources///Struts2///src///struts-2.3.30,"
+		".src///test///resources///BatchClusteringEngineTest_resources///struts-2.3.30_deps.rsf,"
 		+ "java,"
-		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.3.30///arc,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.3.30_239_topics_234_arc_clusters.rsf,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.3.30_239_topics_234_arc_docTopics.json,"
 		+ "lib_struts,"
 		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.3.30_arc_smells.ser,"
-		+ "struts-2.3.30_arc_smells.ser",
+		+ ".///target///test_results///BatchClusteringEngineTest///struts-2.3.30_arc_smells.ser",
 
 		// struts 2.5.2
-		".///src///test///resources///subject_systems_resources///Struts2///src///struts-2.5.2,"
+		".src///test///resources///BatchClusteringEngineTest_resources///struts-2.5.2_deps.rsf,"
 		+ "java,"
-		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.5.2///arc,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.5.2_284_topics_275_arc_clusters.rsf,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.5.2_284_topics_275_arc_docTopics.json,"
 		+ "lib_struts,"
 		+ ".///src///test///resources///BatchClusteringEngineTest_resources///struts-2.5.2_arc_smells.ser,"
-		+ "struts-2.5.2_arc_smells.ser",
+		+ ".///target///test_results///BatchClusteringEngineTest///struts-2.5.2_arc_smells.ser",
 
 		// httpd 2.3.8
-		".///src///test///resources///subject_systems_resources///httpd///src///httpd-2.3.8,"
+		".src///test///resources///BatchClusteringEngineTest_resources///httpd-2.3.8_deps.rsf,"
 		+ "c,"
-		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.3.8///arc,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.3.8_46_topics_71_arc_clusters.rsf,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.3.8_46_topics_71_arc_docTopics.json,"
 		+ "/,"
 		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.3.8_arc_smells.ser,"
-		+ "httpd-2.3.8_arc_smells.ser",
+		+ ".///target///test_results///BatchClusteringEngineTest///httpd-2.3.8_arc_smells.ser",
 
 		// httpd 2.4.26
-		".///src///test///resources///subject_systems_resources///httpd///src///httpd-2.4.26,"
+		".src///test///resources///BatchClusteringEngineTest_resources///httpd-2.4.26_deps.rsf,"
 		+ "c,"
-		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.4.26///arc,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.4.26_50_topics_82_arc_clusters.rsf,"
+		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.4.26_50_topics_82_arc_docTopics.json,"
 		+ "/,"
 		+ ".///src///test///resources///BatchClusteringEngineTest_resources///httpd-2.4.26_arc_smells.ser,"
-		+ "httpd-2.4.26_arc_smells.ser"
+		+ ".///target///test_results///BatchClusteringEngineTest///httpd-2.4.26_arc_smells.ser",
 	})
-	public void asdWithConcernsTest(String sysVersionDir, String lang,
-			String testOutputDir, String classesDir, String oraclePath,
-			String arcFilename){
+	public void asdWithConcernsTest(String depsRsfFilePath, String lang,
+			String clusterFilePath, String docTopicsFilePath, String classesDir,
+			String oraclePath, String arcFilename) {
 		/** ARC - smell analyzer integration test **/
 		// Format paths
-		String sysDir = sysVersionDir.replace("///", File.separator);
-		String outputDirName = testOutputDir.replace("///", File.separator);
+		String depsRsfFile = depsRsfFilePath.replace("///", File.separator);
+		String docTopicsPath = docTopicsFilePath.replace("///", File.separator);
 		String oracleFilePath = oraclePath.replace("///", File.separator);
+		String clusterFile = clusterFilePath.replace("///", File.separator);
+		String resultFile = arcFilename.replace("///", File.separator);
 
 		assertDoesNotThrow(() -> {
-			BatchClusteringEngine.single(new File(sysDir), lang, outputDirName, classesDir); // calls ArchSmellDetector.run(true,true,true)
+			TopicUtil.docTopics = DocTopics.deserializeDocTopics(docTopicsPath);
+			ArchSmellDetector asd = new ArchSmellDetector(
+				depsRsfFile, clusterFile, resultFile, lang,
+				TopicModelExtractionMethod.MALLET_API, TopicUtil.docTopics);
+			asd.run(true, true, true);
 		});
 
-		String resultFilePath = outputDirName + File.separatorChar + arcFilename;
-
 		// Construct SmellCollection objects out of the oracle and result files
-		SmellCollection resultSmells = assertDoesNotThrow(() -> { return new SmellCollection(resultFilePath); });
+		SmellCollection resultSmells = assertDoesNotThrow(() -> { return new SmellCollection(resultFile); });
 		SmellCollection oracleSmells = assertDoesNotThrow(() -> { return new SmellCollection(oracleFilePath); });
 
 		// SmellCollection extends HashSet, so we can use equals() to compare the result to the oracle
