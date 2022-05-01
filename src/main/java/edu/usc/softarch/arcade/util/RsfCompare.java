33b75d131e071463eeaf7321741dad88c62383a9
MarceloLaser <marcelo.laser@gmail.com>
2022-05-01 12:47:15 -0700

    added main call to RsfCompare

--- src/main/java/edu/usc/softarch/arcade/util/RsfCompare.java
+++ src/main/java/edu/usc/softarch/arcade/util/RsfCompare.java
@@ -1,10 +1,23 @@
 package edu.usc.softarch.arcade.util;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RsfCompare implements Comparable<RsfCompare> {
+	public static void main(String[] args) throws IOException {
+		String rsf1Path = args[0];
+		String rsf2Path = args[1];
+		String rsf1 = FileUtil.readFile(rsf1Path, StandardCharsets.UTF_8);
+		String rsf2 = FileUtil.readFile(rsf2Path, StandardCharsets.UTF_8);
+		RsfCompare rsf1Compare = new RsfCompare(rsf1);
+		RsfCompare rsf2Compare = new RsfCompare(rsf2);
+		System.out.println(rsf1Compare.equals(rsf2Compare));
+	}
+
 	private Set<String> rsfSet;
 	
 	// Constructor passes in string (pass in path to FileUtil.readFile)
@@ -27,5 +40,14 @@ public class RsfCompare implements Comparable<RsfCompare> {
 
 		RsfCompare toCompare = (RsfCompare)o;
 		return this.rsfSet.equals(toCompare.rsfSet);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		for (String s : rsfSet.stream().sorted().collect(Collectors.toList()))
+			result.append(s).append(System.lineSeparator());
+
+		return result.toString();
 	}
 }
